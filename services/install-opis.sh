2feffe2b8db3ef8c533fff7edf173fe739e2043a
Marcell Nagy <marcell.nagy@diamond.ac.uk>
2023-11-08 14:19:21 +0000

    Create beamline step 3.4

--- services/install-opis.sh
+++ services/install-opis.sh
@@ -2,4 +2,4 @@
 
 # Install a nginx server to publish GUI files for the beamline
 helm upgrade --install opis oci://ghcr.io/epics-containers/domain-opis \
-     --version 2.0.0 --set beamline=blxxi
+     --version 2.0.0 --set beamline=bl01t
--- services/install-pvcs.sh
+++ services/install-pvcs.sh
@@ -2,4 +2,4 @@
 
 # Install a shared pvcs for the beamline - do this before any other services
 helm upgrade --install shared-pvcs oci://ghcr.io/epics-containers/domain-shared-pvcs \
-     --version 2.0.0 --set beamline=blxxi
+     --version 2.0.0 --set beamline=bl01t
